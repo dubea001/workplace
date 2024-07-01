8b4229abbf7fcec1b7b2a4f73f1f45b68e47bbcf
Dubea <dubea001@gmail.com>
2024-07-01 14:31:48 +0100

    modified the project 7 html,css and js  file

--- project 7/script.js
+++ project 7/script.js
@@ -1,18 +1,23 @@
 const userPickDisplay = document.querySelector('.user-pick');
 const computerPickDisplay = document.querySelector('.computer-pick');
+const rockBtn = document.querySelector('#rock')
+const paperBtn = document.querySelector('#paper')
+const scissorsBtn = document.querySelector('#scissors')
+
+const resultDisplay = document.querySelector('#result')
 
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3) + 1;
   let computerChoice;
   switch (randomNumber) {
     case 1:
-      computerChoice = 'Rock';
+      computerChoice = 'rock';
       break;
     case 2:
-      computerChoice = 'Paper';
+      computerChoice = 'paper';
       break;
     case 3:
-      computerChoice = 'Scissors';
+      computerChoice = 'scissors';
       break;
     default:
       break;
@@ -71,5 +76,24 @@ const playGame = (userChoice) => {
     break;
     default:
     break;
+    
   }
+
+  
+
+  resultDisplay.innerHTML = `<h1>${result}</h1>`
+
+  userPickDisplay.innerHTML = `<h1>player picked ${userChoice}</h1>`
+  computerPickDisplay.innerHTML = `<h1>computer picked ${computerMove}</h1>`
+  return result
 }
+
+rockBtn.addEventListener('click', () => {
+  playGame('rock')
+})
+paperBtn.addEventListener('click', () => {
+  playGame('paper')
+})
+scissorsBtn.addEventListener('click', () => {
+  playGame('scissors')
+})
